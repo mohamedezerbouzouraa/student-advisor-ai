48498b2e0c6e693e956a6c7260822bd247599b86
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-03-19 23:38:46 +0100

    main

---
+++ main.py
@@ -0,0 +1,14 @@
+import asyncio
+import sys
+from interactive import interactive_mode
+
+if __name__ == "__main__":
+    if "ipykernel" in sys.modules:
+        loop = asyncio.get_running_loop()
+        task = loop.create_task(interactive_mode())
+        try:
+            loop.run_until_complete(task)
+        except asyncio.CancelledError:
+            pass
+    else:
+        asyncio.run(interactive_mode())
