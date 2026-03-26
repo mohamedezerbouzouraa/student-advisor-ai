9428ce00ce5958cb216effa3df56e8c87fb49c67
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-03-26 17:07:41 +0100

    defining the asynchronous function to call the agent.

--- agent_caller.py
+++ agent_caller.py
@@ -3,6 +3,7 @@ from runner_setup import runner, USER_ID, SESSION_ID
 
 async def call_agent_async(query: str):
     final_response_text = "No response."
+    
     async for event in runner.run_async(
         user_id=USER_ID,
         session_id=SESSION_ID,
