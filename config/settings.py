933a16810e13a25f9b194a5c83c0f006f0f8cd67
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-03-29 23:19:44 +0200

    Add configuration settings for Google API

--- config/settings.py
+++ config/settings.py
@@ -2,7 +2,6 @@ import os
 import google.generativeai as genai
 
 os.environ["GOOGLE_API_KEY"] = "xxxxxxxxxxxxxxxx"
-
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
 APP_NAME = "adk_course_app"
