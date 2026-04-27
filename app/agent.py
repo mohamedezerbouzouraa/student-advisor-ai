e3d9f7fa0779016ad763bea76acf25228e3fb821
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-04-27 18:55:19 +0100

    Update agent.py

--- app/agent.py
+++ app/agent.py
@@ -2,6 +2,5 @@ from config.settings import AGENT_MODEL, SYSTEM_PROMPT
 import google.generativeai as genai
 
 model = genai.GenerativeModel(AGENT_MODEL)
-
 def build_prompt(user_message: str) -> str:
     return SYSTEM_PROMPT + "\n\nStudent: " + user_message
