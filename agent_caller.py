5a224c68d37808b67866be3359659fef23c18a01
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-05-25 20:59:34 +0100

    update

--- agent_caller.py
+++ agent_caller.py
@@ -2,6 +2,7 @@ import asyncio
 from runner_setup import runner, USER_ID, SESSION_ID
 
 async def call_agent_async(query: str):
+    
     final_response_text = "No response."
     async for event in runner.run_async(
         user_id=USER_ID,
